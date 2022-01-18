7080b55b5c480085ef1c3729bfb3b4d4db185d23
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-18 09:35:18 -0800

    linted typewritter.js

--- typewritter.js
+++ typewritter.js
@@ -9,7 +9,7 @@ for (let i = 0; i < sentence.length; i++) {
     if (i === sentence.length - 1) {
       process.stdout.write('\n');
     }
-  },delay)
+  },delay);
   //console.log(sentence[i])
   delay += 50;
 }
